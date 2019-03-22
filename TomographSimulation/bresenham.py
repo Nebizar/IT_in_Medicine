7ca0a166beaf0b9389251ae42888fb25c849135c
Krzysztof <krzysztof211@gmail.com>
2019-03-22 10:31:31 +0100

    bresenham fixed

--- TomographSimulation/bresenham.py
+++ TomographSimulation/bresenham.py
@@ -5,28 +5,59 @@ Created on Fri Mar 15 11:59:35 2019
 @author: Krzysztof Pasiewicz
 """
 
-def bresenham(x0, y0, x1, y1):
-    dx = x1 - x0
-    dy = y1 - y0
+import numpy as np
 
-    xsign = 1 if dx > 0 else -1
-    ysign = 1 if dy > 0 else -1
+def bresenham(start, end):
+    # step 1 get end-points of line 
+    (x0, y0) = start
+    (x1, y1) = end
 
-    dx = abs(dx)
-    dy = abs(dy)
+    # step 2 calculate difference
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    m = dy/dx
+    
+    # step 3 perform test to check if pk < 0
+    flag = True
+    
+    line_pixel = []
+    line_pixel.append((x0,y0))
+    
+    step = 1
+    if x0>x1 or y0>y1:
+        step = -1
+        
+    if m < 1:
+        x0, x1 ,y0 ,y1 = y0, y1, x0, x1
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        mm = True
+        
+    p0 = 2*dx - dy
+    x = x0
+    y = y0
+    
+    for i in range(abs(y1-y0)):
+        if flag:
+            x_previous = x0
+            p_previous = p0
+            p = p0
+            flag = False
+        else:
+            x_previous = x
+            p_previous = p
+            
+        if p >= 0:
+            x = x + step
 
-    if dx > dy:
-        xx, xy, yx, yy = xsign, 0, 0, ysign
-    else:
-        dx, dy = dy, dx
-        xx, xy, yx, yy = 0, ysign, xsign, 0
-
-    D = 2*dy - dx
-    y = 0
-
-    for x in range(dx + 1):
-        yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
-        if D >= 0:
-            y += 1
-            D -= 2*dx
-        D += 2*dy
+        p = p_previous + 2*dx -2*dy*(abs(x-x_previous))
+        y = y + 1
+        
+        if mm:
+            line_pixel.append((y,x))
+        else:
+            line_pixel.append((x,y))
+            
+    line_pixel = np.array(line_pixel)
+    
+    return line_pixel
